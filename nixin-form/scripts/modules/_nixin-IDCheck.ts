75b2ecf01e4be165179f1266e5abeb44454f6aec
Luigi Senzamici <luigisenzamici@gmail.com>
2017-02-27 23:50:26 +0100

    add _nixin_idcheck (complete), add _regexpr, add documentation with typedoc

---
+++ nixin-form/scripts/modules/_nixin-IDCheck.ts
@@ -0,0 +1,127 @@
+﻿/**
+* Input Descriptor for Check test
+* this module collect all methods for basical test
+*/
+
+import { _regExpr as regExpr } from "./_regExpr";
+/**
+* isEmpty
+* @param value  value to test
+* @return true or false
+*/
+let isEmpty = function (value: any): boolean {
+    if (value != null && value != undefined && value.length != 0) return false;
+    return true;
+}
+/**
+* notEmpty
+* @param value  value to test
+* @return true or false
+*/
+let notEmpty = function (value: any): boolean {
+    if (isEmpty(value)) return false;
+    return true;
+}
+/**
+* isInteger
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isInteger = function (value: any) {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.integer)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isFloat
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isFloat = function(value: any):boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.float)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isNegative
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isNegative = function(value: any):boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.float)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isItalianZipCode
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isItalianZipCode = function (value: any):boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.cap)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isEmail
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isEmail = function (value: any): boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.email)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isGenericPhoneNumber
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isGenericPhoneNumber = function(value: any):boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.genericPhoneNumber)) return true;
+        return false;
+    }
+    return false;
+}
+/**
+* isGenericPhoneNumber
+* @param value  value to test
+* @return true or false matching a regular expression in _regExpr module
+*/
+let isItalianMobilePhoneNumber = function (value: any): boolean {
+    if (notEmpty(value)) {
+        if (typeof value !== 'string') value = value.toString();
+        if (value.match(regExpr.italianMobilePhone)) return true;
+        return false;
+    }
+    return false;
+}
+export {
+        isEmpty,
+        notEmpty,
+        isInteger,
+        isFloat,
+        isNegative,
+        isItalianZipCode,
+        isEmail,
+        isGenericPhoneNumber,
+        isItalianMobilePhoneNumber
+};
+
--- nixin-form/scripts/modules/_nixin-inputTestingDescriber.ts
+++ nixin-form/scripts/modules/_nixin-inputTestingDescriber.ts
@@ -1,3 +1,6 @@
 /**
- * Created by luigisenzamici on 27/02/17.
- */
+* Input Testing Decriber
+* Module that collect and expose all type testing to validate input form 
+*/
+import { isEmpty, notEmpty, isInteger, isFloat, isNegative, isItalianZipCode,
+    isEmail, isGenericPhoneNumber, isItalianMobilePhoneNumber } from"./_nixin-IDCheck";
