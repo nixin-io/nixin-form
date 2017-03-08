ae6b1a32948a65eead85ed1f090b2edf0269f0e5
Luigi Senzamici <luigi.senzamici@preventivi.it>
2017-03-08 13:01:14 +0100

    rebuild test

--- tests/src/_IDCheck.js
+++ tests/src/_IDCheck.js
@@ -73,18 +73,22 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var _regExpr = {
-    italianMobilePhone: /^([+]39)?((38[{8,9}|0])|(34[{7-9}|0])|(36[6|8|0])|(33[{3-9}|0])|(32[{8,9}]))([\d]{7})$/,
-    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    cap: /^(V-|I-)?[0-9]{5}$/,
-    number: /^-?\d+$/,
-    noNumber: /^([^0-9]*)$/,
-    integer: /^\d+$/,
-    float: /[+-]?([0-9]*[.])?[0-9]+/,
-    negative: /^-\d*\.?\d+$/,
-    genericPhoneNumber: /(^[0-9\/\\]*)$/
-};
+var _regExpr = (function () {
+    function _regExpr() {
+        this.italianMobilePhone = /^([+]39)?((38[{8,9}|0])|(34[{7-9}|0])|(36[6|8|0])|(33[{3-9}|0])|(32[{8,9}]))([\d]{7})$/;
+        this.email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.cap = /^(V-|I-)?[0-9]{5}$/;
+        this.number = /^-?\d+$/;
+        this.noNumber = /^([^0-9]*)$/;
+        this.integer = /^\d+$/;
+        this.float = /[+-]?([0-9]*[.])?[0-9]+/;
+        this.negative = /^-\d*\.?\d+$/;
+        this.genericPhoneNumber = /(^[0-9\/\\]*)$/;
+    }
+    return _regExpr;
+}());
 exports._regExpr = _regExpr;
+;
 
 
 /***/ }),
@@ -99,139 +103,140 @@ exports._regExpr = _regExpr;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 var _regExpr_1 = __webpack_require__(0);
+var regExpr = new _regExpr_1._regExpr();
 /**
 * isEmpty
 * @param value  value to test
 * @return true or false
 */
-var isEmpty = function (value) {
+function isEmpty(value) {
     if (value != null && value != undefined && value.length != 0)
         return false;
     return true;
-};
+}
 exports.isEmpty = isEmpty;
 /**
 * notEmpty
 * @param value  value to test
 * @return true or false
 */
-var notEmpty = function (value) {
+function notEmpty(value) {
     if (isEmpty(value))
         return false;
     return true;
-};
+}
 exports.notEmpty = notEmpty;
 /**
 * isInteger
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isInteger = function (value) {
+function isInteger(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.integer))
+        if (value.match(regExpr.integer))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isInteger = isInteger;
 /**
 * isFloat
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isFloat = function (value) {
+function isFloat(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.float))
+        if (value.match(regExpr.float))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isFloat = isFloat;
 /**
 * isNegative
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isNegative = function (value) {
+function isNegative(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.float))
+        if (value.match(regExpr.float))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isNegative = isNegative;
 /**
 * isItalianZipCode
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isItalianZipCode = function (value) {
+function isItalianZipCode(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.cap))
+        if (value.match(regExpr.cap))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isItalianZipCode = isItalianZipCode;
 /**
 * isEmail
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isEmail = function (value) {
+function isEmail(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.email))
+        if (value.match(regExpr.email))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isEmail = isEmail;
 /**
 * isGenericPhoneNumber
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isGenericPhoneNumber = function (value) {
+function isGenericPhoneNumber(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.genericPhoneNumber))
+        if (value.match(regExpr.genericPhoneNumber))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isGenericPhoneNumber = isGenericPhoneNumber;
 /**
 * isGenericPhoneNumber
 * @param value  value to test
 * @return true or false matching a regular expression in _regExpr module
 */
-var isItalianMobilePhoneNumber = function (value) {
+function isItalianMobilePhoneNumber(value) {
     if (notEmpty(value)) {
         if (typeof value !== 'string')
             value = value.toString();
-        if (value.match(_regExpr_1._regExpr.italianMobilePhone))
+        if (value.match(regExpr.italianMobilePhone))
             return true;
         return false;
     }
     return false;
-};
+}
 exports.isItalianMobilePhoneNumber = isItalianMobilePhoneNumber;
 
 
